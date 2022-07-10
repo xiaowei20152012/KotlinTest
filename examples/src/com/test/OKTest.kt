964c7d5df564ff20a860335996d76e62dc03a7cc
liwei <wei.li@zenjoy.net>
2022-07-10 15:20:41 +0800

    [LW] add test code.

--- examples/src/com/test/OKTest.kt
+++ examples/src/com/test/OKTest.kt
@@ -1,5 +1,15 @@
 package com.test
 
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+
 fun main() {
-    
+    val okHttpClient = OkHttpClient()
+    val okHttpClientCustom = OkHttpClient.Builder().build()
+
+    val request = Request.Builder().get().url("https://www.kotlincn.net/docs/reference/keyword-reference.html").build()
+    val response = okHttpClient.newCall(request).execute()
+
+    System.out.println(response.message)
 }
